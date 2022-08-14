089c03292639d3e7d3379703357edc7626e851a0
balu <balu1072000@gmail.com>
2022-08-14 13:10:06 +0530

    changes done

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/ang-quiz1'));
+app.use(express.static(__dirname + '/dist/'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/ang-quiz1/index.html'));});
+'/dist/index.html'));});
 app.listen(process.env.PORT || 8080);
